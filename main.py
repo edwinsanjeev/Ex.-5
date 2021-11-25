6a5bf64146bb21f4b8c546af924305199fedeaa5
Edwin Sanjeev <94860839+edwinsanjeev@users.noreply.github.com>
2021-11-25 17:32:35 +0000

    Had to Minus semi with side C

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 # Date: 11/25/2021
 # Description: Exercise 05 (Triangle)
 from math import *
+
+
 side_a = float(input("Enter side A: "))
 side_b = float(input("Enter side B: "))
 side_c = float(input("Enter side C: "))
@@ -9,7 +11,7 @@ perimeter = side_a + side_b + side_c
 print()
 semi = perimeter /2
 # Heron's Formula
-area = sqrt(semi*(semi-side_a)*(semi-side_b)*(side_c))
+area = sqrt(semi*(semi-side_a)*(semi-side_b)*(semi-side_c))
 
 print(f"The perimeter is {perimeter:,.3f}.")
 print()
